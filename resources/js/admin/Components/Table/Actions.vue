e22afe40c7e143e269ad95adbe8bdad0116fffb9
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-09-12 18:51:03 +0300

    Added alert pop up

---
+++ resources/js/admin/Components/Table/Actions.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import Edit from "@/Components/Icons/Edit.vue";
+import Delete from "@/Components/Icons/Delete.vue";
+
+defineProps({
+    editLink: {
+        type: String
+    },
+    showEdit: {
+        type: Boolean,
+        default: () => true
+    },
+    deleteLink: {
+        type: String
+    },
+    showDelete: {
+        type: Boolean,
+        default: () => true
+    },
+})
+</script>
+<template>
+    <div class="flex items-center space-x-2">
+        <button v-if="showEdit">
+            <Link :href="editLink">
+                <Edit class="w-4 h-4"/>
+            </Link>
+        </button>
+        <button v-if="showDelete">
+            <Link :href="deleteLink">
+                <Delete  class="w-4 h-4"/>
+            </Link>
+        </button>
+    </div>
+</template>
